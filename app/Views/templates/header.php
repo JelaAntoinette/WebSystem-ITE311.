9a7266720adbf349276f8e63125d4a9de00a667e
Jela Atoinette <jelajeminez8@gmail.com>
2025-09-26 01:07:06 +0800

    some changes

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,6 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-info">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
         <a class="navbar-brand" href="<?= base_url('dashboard') ?>">My Website</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-label="Toggle">
@@ -10,8 +10,8 @@
         <div class="collapse navbar-collapse" id="nav">
             <ul class="navbar-nav ms-auto">
                 <?php if ($user['role'] === 'admin'): ?>
-                 
-                  
+                    <li class="nav-item"><a class="nav-link" href="<?= base_url('/users') ?>">Manage Users</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= base_url('/reports') ?>">Reports</a></li>
                 <?php elseif ($user['role'] === 'teacher'): ?>
                     <li class="nav-item"><a class="nav-link" href="<?= base_url('/classes') ?>">My Classes</a></li>
                 <?php else: ?>
