1e6edd525b1964893c24716e8f0c1f610f3c9331
Jela Atoinette <jelajeminez8@gmail.com>
2025-09-26 01:44:19 +0800

    some changes

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="<?= base_url('dashboard') ?>">My Website</a>
+        <a class="navbar-brand" href="<?= base_url('dashboard') ?>">LMS</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-label="Toggle">
             <span class="navbar-toggler-icon"></span>
         </button>
